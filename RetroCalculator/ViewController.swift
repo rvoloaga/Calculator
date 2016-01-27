4951e5e5e306d759e6a93deae8f29d4123d31209
Radu Voloaga <radu@voloaga.com>
2016-01-27 13:31:50 +0200

    Calculator
    basic calculator

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,19 +7,141 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    enum Operation: String {
+        
+        case Divide = "/"
+        case Multiply = "*"
+        case Substract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+        case Clear = "Clear"
+        
+    }
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftVarStr = ""
+    var rightVarStr = ""
+    var currentOperation : Operation = Operation.Empty
+    var result = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do {
+            
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            
+        }catch let err as NSError {
+            
+            print(err.debugDescription)
+            
+        }
+        
+        
+        
+    }
+    
+    @IBAction func numberPressed(btn: UIButton!) {
+        
+        playSound()
+        
+        runningNumber += "\(btn.tag)"
+        
+        outputLbl.text = runningNumber
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(Operation.Divide)
+    }
+   
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(Operation.Multiply)
     }
 
+    @IBAction func onSubstractPressed(sender: AnyObject) {
+        processOperation(Operation.Substract)
+    }
 
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(Operation.Add)
+    }
+    
+    @IBAction func onEqualsPressed(sender: AnyObject) {
+        processOperation(currentOperation)
+    }
+    
+    @IBAction func onClearPressed(sender: AnyObject) {
+        processOperation(Operation.Clear)
+    }
+    
+    func processOperation(op: Operation) {
+        
+        playSound()
+        
+        if currentOperation != Operation.Empty {
+            
+            if runningNumber != "" {
+                
+                rightVarStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    
+                    result = "\(Double(leftVarStr)! * Double(rightVarStr)!)"
+                    
+                } else   if currentOperation == Operation.Divide {
+                    
+                    result = "\(Double(leftVarStr)! / Double(rightVarStr)!)"
+                    
+                } else   if currentOperation == Operation.Substract {
+                    
+                    result = "\(Double(leftVarStr)! - Double(rightVarStr)!)"
+                    
+                } else   if currentOperation == Operation.Add {
+                    
+                    result = "\(Double(leftVarStr)! + Double(rightVarStr)!)"
+                    
+                }else if currentOperation == Operation.Clear {
+               
+                    
+                
+                }
+                
+                leftVarStr = result
+                outputLbl.text = result
+                
+            }
+            
+            currentOperation = op
+        
+        } else {
+            
+            leftVarStr = runningNumber
+            runningNumber = ""
+            currentOperation = op
+            
+        }
+    
+    }
+    
+    func playSound () {
+        if btnSound.playing {
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
 }
 
